29509fd20ed8d4901641845381a88716aa4c79f2
unknown <heidysagnip2929@gamil.com>
2019-11-14 13:10:51 -0800

    First version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,7 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule,
-    HttpClient
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
